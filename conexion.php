b0e1afdddbc231eeb9842cf9aedf180e67d6e76b
javiermiras88 <javiermiras88@hotmail.com>
2018-05-22 12:39:53 +0000

    Create conexion.php

--- conexion.php
+++ conexion.php
@@ -7,7 +7,7 @@ $enlace = mysqli_connect('localhost:3306', 'root','', 'mercamira');
 
 if(!$enlace){
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_error() . PHP_EOL;
     echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
